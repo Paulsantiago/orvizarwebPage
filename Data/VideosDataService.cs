e0b8889212f7ca6d942f78025d03dbc56f3b995a
pstumbaco <paultumbaco@gmail.com>
2020-10-12 07:51:58 +0800

    second

---
+++ Data/VideosDataService.cs
@@ -0,0 +1,74 @@
+﻿using OrvizarWebPageServer.SharedFolder;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrvizarWebPageServer.Data
+{
+
+    public enum PlayList
+    {
+        Python,
+        Anaconda,
+        MatplotLib,
+        Pandas,
+        Csharp,
+        Unity,
+        Opencv,
+        Optimizacion,
+
+    }
+    public class VideosDataService
+    {
+        public PlayList playList { get; set; }
+        public SourceElements elements { get; set; }
+
+        public Videos video { get; set; }
+        public SourceElements ListVideos(PlayList playList)
+        {
+            elements = new SourceElements();
+            switch (playList)
+            {
+                case PlayList.Anaconda:
+                    elements.AllVideos = new List<Videos>
+                    {
+
+                        new Videos  {  Name = @"https://www.youtube.com/embed/_ihdkS8jBvA"},
+                        new Videos  {  Name = @"https://www.youtube.com/embed/_ihdkS8jBvA" },
+
+                    };
+                    return elements;
+                case PlayList.Python:
+                    elements.AllVideos = new List<Videos>
+                    {
+
+                        new Videos  {  Name = "https://www.youtube.com/embed/videoseries?list=PL6GZzVvsfrGEDJ5oOLO210mP1MSj13PFX" },
+                        new Videos  {  Name = "https://www.youtube.com/embed/videoseries?list=PL6GZzVvsfrGEDJ5oOLO210mP1MSj13PFX" },
+
+                    };
+                    return elements;
+                case PlayList.MatplotLib:
+                    break;
+                case PlayList.Pandas:
+                    break;
+                case PlayList.Csharp:
+                    break;
+                case PlayList.Unity:
+                    break;
+                case PlayList.Opencv:
+                    break;
+                default:
+                    break;
+
+            }
+            return null;
+        }
+
+        public Task<SourceElements> GetVideosAsync(PlayList playL)
+        {
+            return Task.FromResult(ListVideos(playL));
+        }
+    }
+}
---
+++ SharedFolder/SourceElements.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OrvizarWebPageServer.SharedFolder
+{
+    public class SourceElements
+    {
+        public List<Videos> AllVideos { get; set; }
+        public Videos GetVideoById(int Id) => AllVideos.Where(video => video.Id == Id).FirstOrDefault();
+    }
+}
